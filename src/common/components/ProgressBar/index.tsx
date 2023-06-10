aed5a70b937a564d84e1b55234b29578d27e5526
Guga Villar <grsv.21@gmail.com>
2023-06-10 14:47:46 -0300

    :sparkles: Feat: integration progress bar

--- src/common/components/ProgressBar/index.tsx
+++ src/common/components/ProgressBar/index.tsx
@@ -1,10 +1,14 @@
 import { Box, Progress, Text } from '@chakra-ui/react'
+import { memo } from 'react'
 
 type ProgressBarProps = {
   value: number
+  total: number
 }
 
-export const ProgressBar = ({ value }: ProgressBarProps) => {
+export const ProgressBar = memo(({ value, total }: ProgressBarProps) => {
+  const progressValue = (value / total) * 100
+  const textWidthValue = progressValue === 0 ? '25px' : `${progressValue}%`
   return (
     <Box
       px={[8, 8, 8, '6.9375rem']}
@@ -12,8 +16,12 @@ export const ProgressBar = ({ value }: ProgressBarProps) => {
       my={[8, 8, 12]}
       mx="auto"
     >
-      <Text mb={4}>{value}%</Text>
-      <Progress value={value} borderRadius={8} />
+      <Text mb={4} width={textWidthValue} textAlign="end">
+        {progressValue}%
+      </Text>
+      <Progress isAnimated value={progressValue} borderRadius={8} />
     </Box>
   )
-}
+})
+
+ProgressBar.displayName = 'ProgressBar'
