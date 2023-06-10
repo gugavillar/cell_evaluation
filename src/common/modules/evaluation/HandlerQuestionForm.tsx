61d81cb5a1f36ea001d668cf42ef70b805d3ea3c
Guga Villar <grsv.21@gmail.com>
2023-06-10 14:51:12 -0300

    :sparkles: Integration answers with progress bar

--- src/common/modules/evaluation/HandlerQuestionForm.tsx
+++ src/common/modules/evaluation/HandlerQuestionForm.tsx
@@ -1,4 +1,7 @@
-import { QuestionForm } from '@/common/components'
+import { ProgressBar, QuestionForm } from '@/common/components'
+import { isNullOrUndefined } from '@/common/formatters/values'
+
+import { Fragment, useMemo } from 'react'
 import { useForm } from 'react-hook-form'
 
 const questions = [
@@ -6,23 +9,43 @@ const questions = [
   'Como você avalia o conhecimento do líder',
 ]
 
+type HandlerQuestionFormType = {
+  [key: string]: string
+}
+
 export const HandlerQuestionForm = () => {
   const {
     register,
     handleSubmit,
+    watch,
     formState: { isValid, isDirty, isSubmitting },
-  } = useForm()
+  } = useForm<HandlerQuestionFormType>()
+
+  const formValues = watch()
+
+  const valueOfProgress = useMemo(
+    () =>
+      Object.values(formValues)?.reduce(
+        (acc, formValue) => (isNullOrUndefined(formValue) ? acc : acc + 1),
+        0,
+      ),
+    [formValues],
+  )
+
   const handleConcludeEvaluation = (values: any) => console.log(values)
 
   const isInvalidForm = !isDirty || !isValid
 
   return (
-    <QuestionForm
-      handleSubmit={handleSubmit(handleConcludeEvaluation)}
-      isInvalidForm={isInvalidForm}
-      isSendingForm={isSubmitting}
-      questions={questions}
-      register={register}
-    />
+    <Fragment>
+      <ProgressBar value={valueOfProgress} total={questions?.length} />
+      <QuestionForm
+        handleSubmit={handleSubmit(handleConcludeEvaluation)}
+        isInvalidForm={isInvalidForm}
+        isSendingForm={isSubmitting}
+        questions={questions}
+        register={register}
+      />
+    </Fragment>
   )
 }
