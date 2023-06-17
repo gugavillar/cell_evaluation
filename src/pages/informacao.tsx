c79cc614a889c4dc1cb8e871a84a37cb84c43fbc
Guga Villar <grsv.21@gmail.com>
2023-06-17 11:27:37 -0300

    :sparkles: Feat: information page

---
+++ src/pages/informacao.tsx
@@ -0,0 +1,40 @@
+import { useRouter } from 'next/router'
+import { Fragment } from 'react'
+
+import {
+  Box,
+  Card,
+  CardBody,
+  Heading,
+  Icon,
+  Text,
+  VStack,
+} from '@chakra-ui/react'
+import { WarningCircle } from 'phosphor-react'
+
+import { Header } from '@/common/components'
+
+export default function Information() {
+  const router = useRouter()
+
+  const { message } = router.query
+
+  return (
+    <Fragment>
+      <Header title="Igreja Anglicana Vida" subtitle="" />
+      <Box maxW={['full', '48rem', '54rem']} p={6} mx="auto">
+        <Card mt={-20}>
+          <CardBody>
+            <VStack spacing={4}>
+              <Icon as={WarningCircle} w={20} h={20} color="brand.red" />
+              <Heading fontSize={['xl', '2xl', '3xl']}>
+                Algo errado aconteceu!
+              </Heading>
+              <Text textAlign="center">{message}</Text>
+            </VStack>
+          </CardBody>
+        </Card>
+      </Box>
+    </Fragment>
+  )
+}
